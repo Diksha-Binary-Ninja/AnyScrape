9fdc15a2642e1965fce4ccdffaa691737433ddb4
Diksha-Binary-Ninja <139619268+Diksha-Binary-Ninja@users.noreply.github.com>
2024-11-20 22:07:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="A one-liner Python library to scrape headlines, links, and images from any webpage.",
     long_description=open("README.md").read(),  # Include README as long description
     long_description_content_type="text/markdown",
-    author='Diksha, Ayush',
+    author='Diksha, Ayush Kumar Verma',
     author_email='diksha260303official@gmail.com , vermaayush5535@gmail.com',
     url='https://github.com/Diksha-Binary-Ninja/AnyScrape',
     packages=find_packages(),
